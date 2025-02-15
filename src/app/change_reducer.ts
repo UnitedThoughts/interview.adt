80cc81de8f71a4d42463c72a2215495cf14c890b
lightningthought <45131866+lightningthought@users.noreply.github.com>
2025-02-15 22:13:49 +0000

    FIX: #001 - Amounts update one action behind

--- src/app/change_reducer.ts
+++ src/app/change_reducer.ts
@@ -1,25 +1,29 @@
 import Amounts from "./amounts"
 
-export default (amounts: Amounts, action: {type: string, value: number}) => {  
+export default (old_amounts: Amounts, action: {type: string, value: number}) => {  
+
+    let new_amounts: Amounts = {
+      totalCostUSD: old_amounts.totalCostUSD,
+      amountProvidedUSD: old_amounts.amountProvidedUSD,
+      changeOwedUSD: 0,
+      valid: false
+    }
     switch(action.type){
       case 'change:cost': {
-        return {
-          totalCostUSD: action.value,
-          amountProvidedUSD: amounts.amountProvidedUSD,
-          changeOwedUSD: amounts.amountProvidedUSD - amounts.totalCostUSD,
-          valid: (amounts.amountProvidedUSD - amounts.totalCostUSD) >= 0
-        }
+        new_amounts.totalCostUSD = action.value
+        break
       }
       case 'change:amount_provided': {
-        return {
-          totalCostUSD: amounts.totalCostUSD,
-          amountProvidedUSD: action.value,
-          changeOwedUSD: amounts.amountProvidedUSD - amounts.totalCostUSD,
-          valid: (amounts.amountProvidedUSD - amounts.totalCostUSD) >= 0
-        }
+        new_amounts.amountProvidedUSD = action.value
+        break
       }
       default: {
         throw new Error(`Unknown action '${action.type}'`)
       }
     }
+
+    new_amounts.changeOwedUSD = new_amounts.amountProvidedUSD - new_amounts.totalCostUSD
+    new_amounts.valid = new_amounts.changeOwedUSD >= 0
+
+    return new_amounts
   }
