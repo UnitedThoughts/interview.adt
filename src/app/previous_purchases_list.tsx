ece8bff88241253dc7d469e413a69d06ec0533c2
lightningthought <45131866+lightningthought@users.noreply.github.com>
2025-02-17 03:12:46 +0000

    Show newest purchase on top

--- src/app/previous_purchases_list.tsx
+++ src/app/previous_purchases_list.tsx
@@ -4,7 +4,7 @@ import styles            from "./page.module.css"
 
 export default ({amounts}: {amounts: Amounts}) => {
     return <div className={styles.previous_purchases}>
-        {amounts.purchases.map((purchase) => {
+        {amounts.purchases.toReversed().map((purchase) => {
             return <div key={purchase.id} className={styles.purchase}>
                 <span className="purchase_name">{purchase.name}</span>
                 <DenominationsList amounts={purchase.amounts}/>
